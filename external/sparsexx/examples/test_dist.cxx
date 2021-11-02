d443c968f3f1659d89721f41b7fc9d73c925ab1b
David Williams-Young <dbwy@uw.edu>
2021-11-02 11:09:07 -0700

    Cleanup of extraneous code

--- external/sparsexx/examples/test_dist.cxx
+++ external/sparsexx/examples/test_dist.cxx
@@ -45,8 +45,7 @@ int main(int argc, char** argv) {
   const int N = A.m();
 
   sparsexx::dist_sparse_matrix<spmat_type> A_dist( MPI_COMM_WORLD, A );
-  //auto spmv_info = A_dist.get_spmv_info();
-  auto spmv_info = generate_spmv_comm_info( A_dist );
+  auto spmv_info = sparsexx::spblas::generate_spmv_comm_info( A_dist );
 
   // Serial SPMV
   std::vector<double> V(N), AV(N);
@@ -61,112 +60,14 @@ int main(int argc, char** argv) {
   std::iota( V_dist.begin(), V_dist.end(), A_dist.local_row_start() );
   for( auto& x : V_dist ) x *= 0.01;
 
-#if 0
-  // **** Diagonal part ****
-  sparsexx::spblas::gespmbv(1, 1., A_dist.diagonal_tile(), V_dist.data(), N,
-    0., AV_dist.data(), N );
-
-  // **** Off diagonal part ****
-
-  // Post recv for data needed
-  std::vector<MPI_Request> recv_reqs;
-  #if 0
-  std::vector<std::vector<double>> V_recv( world_size );
-  for( auto i = 0; i < world_size; ++i ) 
-  if( i != world_rank ) {
-    const auto& indices = spmv_info.recv_indices[i];
-    auto pack_sz = indices.size();
-    if(pack_sz) {
-      recv_reqs.emplace_back();
-      V_recv[i].resize( pack_sz );
-      MPI_Irecv( V_recv[i].data(), pack_sz, MPI_DOUBLE, i, 0,
-        MPI_COMM_WORLD, &recv_reqs.back() );
-    }
-  }
-  #else
-  size_t nrecv_pack = std::accumulate( spmv_info.recv_counts.begin(),
-                                       spmv_info.recv_counts.end(), 0ul );
-  std::vector<double> V_recv_pack(nrecv_pack);
-  for( auto i = 0; i < world_size; ++i ) 
-  if( i != world_rank and spmv_info.recv_counts[i] ) {
-    recv_reqs.emplace_back();
-    MPI_Irecv( V_recv_pack.data() + spmv_info.recv_offsets[i],
-               spmv_info.recv_counts[i], MPI_DOUBLE, i, 0,
-               MPI_COMM_WORLD, &recv_reqs.back() );
-  }
-  #endif
-
-  // Pack data to send
-  #if 0
-  std::vector<std::vector<double>> V_pack( world_size );
-  for( auto i = 0; i < world_size; ++i )
-  if( i != world_rank ) {
-    const auto& indices = spmv_info.send_indices[i];
-    auto pack_sz = indices.size();
-    if( pack_sz ) {
-      V_pack[i].resize( pack_sz );
-      for( auto j = 0; j < pack_sz; ++j ) {
-        V_pack[i][j] = V_dist[ indices[j] - A_dist.local_row_start() ];
-      }
-      MPI_Request req;
-      MPI_Isend( V_pack[i].data(), pack_sz, MPI_DOUBLE, i, 0,
-        MPI_COMM_WORLD, &req );
-      MPI_Request_free(&req);
-    }
-  }
-  #else
-  size_t nsend_pack = std::accumulate( spmv_info.send_counts.begin(),
-                                       spmv_info.send_counts.end(), 0ul );
-  std::vector<double> V_send_pack(nsend_pack);
-
-  // Pack data
-  for( auto i = 0; i < nsend_pack; ++i ) {
-    V_send_pack[i] = V_dist[ spmv_info.send_indices[i] - A_dist.local_row_start() ]; 
-  }
-
-  // Send data
-  for( auto i = 0; i < world_size; ++i ) 
-  if( i != world_rank and spmv_info.send_counts[i] ) {
-    MPI_Request req;
-    MPI_Isend( V_send_pack.data() + spmv_info.send_offsets[i],
-               spmv_info.send_counts[i], MPI_DOUBLE, i, 0,
-               MPI_COMM_WORLD, &req );
-    MPI_Request_free(&req);
-  }
-  #endif
-
-  // Wait for recvs to be satisfied
-  MPI_Waitall( recv_reqs.size(), recv_reqs.data(), MPI_STATUSES_IGNORE );
-
-  // Unpack into long vector
-  std::vector<double> V_offdiag(N);
-  #if 0
-  for( auto i = 0; i < world_size; ++i )
-  if( i != world_rank ) {
-    const auto& indices = spmv_info.recv_indices[i];
-    auto pack_sz = indices.size();
-    for( auto j = 0; j < pack_sz; ++j ) {
-      V_offdiag[ indices[j] ] = V_recv[i][j];
-    }
-  }
-  #else
-  for( auto i = 0; i < nrecv_pack; ++i ) {
-    V_offdiag[ spmv_info.recv_indices[i] ] = 
-    V_recv_pack[i];
-  }
-  #endif
-
-  sparsexx::spblas::gespmbv(1, 1., A_dist.off_diagonal_tile(), V_offdiag.data(), N,
-    1., AV_dist.data(), N );
-#else
 
   sparsexx::spblas::pgespmv( 1., A_dist, V_dist.data(), 0., AV_dist.data(), 
     spmv_info );
 
-#endif
 
 
 
+  // Compare results
   std::vector<double> AV_dist_combine(N);
   size_t n_per_rank = N / world_size;
   MPI_Allgather( AV_dist.data(), n_per_rank, MPI_DOUBLE, AV_dist_combine.data(),
--- external/sparsexx/include/sparsexx/spblas/pspmbv.hpp
+++ external/sparsexx/include/sparsexx/spblas/pspmbv.hpp
@@ -7,6 +7,157 @@
 
 namespace sparsexx::spblas {
 
+template <typename IndexType>
+struct spmv_info {
+
+  using index_type = IndexType;
+
+  MPI_Comm comm;
+
+  //std::vector< std::vector<index_type> > send_indices;
+  //std::vector< std::vector<index_type> > recv_indices;
+
+  std::vector< index_type > send_indices;
+  std::vector< index_type > recv_indices;
+  std::vector< size_t > send_offsets;
+  std::vector< size_t > recv_offsets;
+  std::vector< size_t > send_counts;
+  std::vector< size_t > recv_counts;
+
+};
+
+template <typename DistSpMatrixType>
+auto generate_spmv_comm_info( const DistSpMatrixType& A ) {
+
+  using index_type = detail::index_type_t<DistSpMatrixType>;
+  auto comm = A.comm();
+  auto comm_size = detail::get_mpi_size(comm);
+  auto comm_rank = detail::get_mpi_rank(comm);
+
+  // Get unique column indices for local rows of A
+  // excluding locally owned elements (i.e. off-diagonal col indices)
+  auto off_diagonal_tile = A.off_diagonal_tile_ptr();
+  std::set<index_type> unique_elements_set;
+  if( off_diagonal_tile ) {
+    assert( off_diagonal_tile->indexing() == 0 );
+    unique_elements_set.insert( off_diagonal_tile->colind().begin(),
+                                off_diagonal_tile->colind().end() );
+  }
+
+  // Place unique col indices into contiguous memory
+  std::vector<index_type> unique_elements( 
+    unique_elements_set.begin(), unique_elements_set.end()
+  );
+
+  // Generate a list of elements that need to be sent by remote
+  // MPI ranks to the current processs
+  std::vector< std::vector<index_type> > recv_indices_by_rank( comm_size );
+  auto uniq_it = unique_elements.begin();
+  for( int i = 0; i < comm_size; ++i ) {
+    // Get row extent for rank-i
+    auto [row_st, row_en] = A.row_bounds(i);
+
+    // Find upper bound for row end
+    auto next_uniq_it = std::lower_bound( uniq_it, unique_elements.end(), 
+      row_en );
+
+    // Copy indices into local memory
+    recv_indices_by_rank[i] = std::vector<index_type>( uniq_it, next_uniq_it );
+    uniq_it = next_uniq_it; // Update iterators
+  }
+
+  // Calculate element counts that will be received from remote processes
+  std::vector<size_t> recv_counts(comm_size);
+  std::transform( recv_indices_by_rank.begin(), recv_indices_by_rank.end(),
+    recv_counts.begin(), [](const auto& idx) {return idx.size();} );
+
+  // Ensure that local recv counts are zero
+  assert( recv_counts[comm_rank] == 0 );
+
+  // Gather recv counts to remote ranks
+  // This tells each rank the number of elements each remote process
+  // expects to receive from the local process
+  std::vector<size_t> recv_counts_gathered( comm_size * comm_size );
+  MPI_Allgather( recv_counts.data(), sizeof(size_t)*comm_size, 
+    MPI_BYTE, recv_counts_gathered.data(), sizeof(size_t)*comm_size,
+    MPI_BYTE, comm );
+
+  
+  // Allocate memory to store the remote indices each remote process
+  // expects to receive from the current process and post async
+  // receives to receive those indices
+  std::vector< std::vector<index_type> > send_indices_by_rank( comm_size );
+  std::vector< MPI_Request > recv_reqs;
+  for( int i = 0; i < comm_size; ++i ) 
+  if( i != comm_rank ) {
+    auto nremote = recv_counts_gathered[ comm_rank + i*comm_size ];
+    if( nremote ) {
+      send_indices_by_rank[i].resize( nremote );
+      auto& req = recv_reqs.emplace_back();
+      MPI_Irecv( send_indices_by_rank[i].data(), nremote * sizeof(index_type),
+        MPI_BYTE, i, 0, comm, &req );
+    }
+  }
+
+  // Send to each remote process the indices it needs to send to the
+  // current process
+  for( auto i = 0; i < comm_size; ++i ) 
+  if( recv_counts[i] ) {
+    MPI_Request req;
+    MPI_Isend( recv_indices_by_rank[i].data(), recv_counts[i]*sizeof(index_type),
+      MPI_BYTE, i, 0, comm, &req );
+    MPI_Request_free(&req);
+  }
+
+  // Wait on receives to complete
+  MPI_Waitall( recv_reqs.size(), recv_reqs.data(), MPI_STATUSES_IGNORE );
+
+
+  // Calculate element counts that will be sent to each remote processes
+  std::vector<size_t> send_counts(comm_size);
+  std::transform( send_indices_by_rank.begin(), send_indices_by_rank.end(),
+    send_counts.begin(), [](const auto& idx) {return idx.size();} );
+
+
+
+  // Compute offsets for send/recv indices in contiguous memory
+  std::vector<size_t> recv_offsets(comm_size), send_offsets(comm_size);
+  std::exclusive_scan( send_counts.begin(), send_counts.end(), 
+    send_offsets.begin(), 0 );
+  std::exclusive_scan( recv_counts.begin(), recv_counts.end(), 
+    recv_offsets.begin(), 0 );
+
+  // Linearize the send/recv data structures
+  size_t nrecv_indices = 
+    std::accumulate( recv_counts.begin(), recv_counts.end(), 0ul );
+  size_t nsend_indices = 
+    std::accumulate( send_counts.begin(), send_counts.end(), 0ul );
+  std::vector<index_type> send_indices, recv_indices;
+  send_indices.reserve( nsend_indices );
+  recv_indices.reserve( nrecv_indices );
+  for( auto i = 0; i < comm_size; ++i ) {
+    send_indices.insert( send_indices.end(), 
+      send_indices_by_rank[i].begin(),
+      send_indices_by_rank[i].end() );
+    recv_indices.insert( recv_indices.end(), 
+      recv_indices_by_rank[i].begin(),
+      recv_indices_by_rank[i].end() );
+  }
+
+    spmv_info<index_type> info;
+    info.comm = comm;
+    info.send_indices = std::move(send_indices);
+    info.recv_indices = std::move(recv_indices);
+    info.recv_offsets = std::move(recv_offsets);
+    info.recv_counts  = std::move(recv_counts);
+    info.send_offsets = std::move(send_offsets);
+    info.send_counts  = std::move(send_counts);
+
+    return info;
+
+
+}
+
 namespace detail {
   using namespace sparsexx::detail;
 }
