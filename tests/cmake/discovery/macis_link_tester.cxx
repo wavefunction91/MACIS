32c46f9ce5673a2ebd2b7e2be584dc3db186d520
David Williams-Young <dbwy@lbl.gov>
2023-05-12 16:13:30 -0700

    macis -> asci

--- tests/cmake/discovery/macis_link_tester.cxx
+++ tests/cmake/discovery/macis_link_tester.cxx
@@ -1,8 +1,8 @@
-#include <asci/fcidump.hpp>
-#include <asci/davidson.hpp>
-#include <asci/hamiltonian_generator/double_loop.hpp>
-#include <asci/util/selected_ci_diag.hpp>
-#include <asci/util/mcscf.hpp>
+#include <macis/fcidump.hpp>
+#include <macis/davidson.hpp>
+#include <macis/hamiltonian_generator/double_loop.hpp>
+#include <macis/util/selected_ci_diag.hpp>
+#include <macis/util/mcscf.hpp>
 int main() {
   return 0;
 }
