783d6059d67be4f308d34779cc86c2c3eb3f6c11
David Williams-Young <dbwy@lbl.gov>
2023-06-01 17:53:38 -0700

    Revamp CI + add missing file

---
+++ include/macis/util/binary_rdms.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+namespace macis {
+
+void read_rdms_binary(std::string fname, size_t norb, double* ORDM, size_t LDD1,
+  double* TRDM, size_t LDD2);
+void write_rdms_binary(std::string fname, size_t norb, const double* ORDM, 
+  size_t LDD1, const double* TRDM, size_t LDD2); 
+
+}
