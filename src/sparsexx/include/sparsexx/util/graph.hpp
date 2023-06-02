9c7e8192980ab43535115a18857a0a5d100d8aad
David Williams-Young <dbwy@lbl.gov>
2023-06-01 18:32:29 -0700

    Additional OpenMP insulation

--- src/sparsexx/include/sparsexx/util/graph.hpp
+++ src/sparsexx/include/sparsexx/util/graph.hpp
@@ -14,7 +14,12 @@
 
 #include <algorithm>
 #include <numeric>
+#ifdef _OPENMP
 #include <omp.h>
+#else
+inline int omp_get_max_threads(){ return 1; }
+inline int omp_get_thread_num() { return 0; }
+#endif
 
 namespace sparsexx {
 
