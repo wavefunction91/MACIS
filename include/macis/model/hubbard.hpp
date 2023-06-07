ebb7ef4d4ee3f92b3b0d23260cfd5cef3ee87f12
Clang Robot <robot@example.com>
2023-06-07 15:57:12 +0000

    Committing clang-format changes

--- include/macis/model/hubbard.hpp
+++ include/macis/model/hubbard.hpp
@@ -4,10 +4,10 @@
 
 namespace macis {
 
-/** 
+/**
  *  @brief Generate Hamiltonian Data for 1D Hubbard
  */
-void hubbard_1d(size_t nsites, double t, double U, std::vector<double>& T, 
+void hubbard_1d(size_t nsites, double t, double U, std::vector<double>& T,
                 std::vector<double>& V);
 
-}
+}  // namespace macis
--- src/macis/model/hubbard.cxx
+++ src/macis/model/hubbard.cxx
@@ -2,28 +2,24 @@
 
 namespace macis {
 
-void hubbard_1d(size_t nsites, double t, double U, std::vector<double>& T, 
+void hubbard_1d(size_t nsites, double t, double U, std::vector<double>& T,
                 std::vector<double>& V) {
-
-
   T.resize(nsites * nsites);
   V.resize(nsites * nsites * nsites * nsites);
 
-
   for(size_t p = 0; p < nsites; ++p) {
     // Half-filling Chemical Potential
-    T[p * (nsites+1)] = -U/2; 
+    T[p * (nsites + 1)] = -U / 2;
 
     // On-Site Interaction
-    V[p * (nsites*nsites*nsites + nsites*nsites + nsites + 1)] = U; 
+    V[p * (nsites * nsites * nsites + nsites * nsites + nsites + 1)] = U;
 
     // Hopping
-    if(p < nsites-1) {
-      T[p + (p+1)*nsites] = -t;
-      T[(p+1) + p*nsites] = -t;
+    if(p < nsites - 1) {
+      T[p + (p + 1) * nsites] = -t;
+      T[(p + 1) + p * nsites] = -t;
     }
   }
-
 }
 
-}
+}  // namespace macis
